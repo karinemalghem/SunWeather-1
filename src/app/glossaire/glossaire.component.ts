3c6292afb94960ea9e03232ce57511b9c3b88f62
Clément Godard <clement_godard@hotmail.fr>
2018-10-14 12:46:33 +0200

    Modification des boutons du glossaire et tris des définitions par ordre alphabétique

--- src/app/glossaire/glossaire.component.ts
+++ src/app/glossaire/glossaire.component.ts
@@ -9,78 +9,12 @@ import { Glossaire } from '../../glossaire';
   styleUrls: ['./glossaire.component.css']
 })
 
-/*class Glossaire
-{
-  id : number;
-  name : string;
-  description : string;
-}*/
-
 export class GlossaireComponent implements OnInit {
 
   public definitions : Glossaire[];
   private glossaireService : GlossaireService;
   
   constructor(glossaire : GlossaireService) { 
-    /*this.definitions = [
-      {
-        id : 0,
-        name : "Sun",
-        description : "A big star that heat us up"
-      },
-      {
-        id : 1,
-        name : "Earth",
-        description : "Our planet, the blue planet"
-      },
-      {
-        id : 2,
-        name : "Space",
-        description : "Infinite space without oxygen"
-      },
-      {
-        id : 3,
-        name : "Clément",
-        description : "A specimen living on the moon"
-      }
-    ];*/
-
-    //console.log(glossaire.getJSON());
-    //let obj : Glossaire[];
-    //let name : string;
-    //let definitions : any[] = [];
-    /*glossaire.getJSON().subscribe((res : any[])=>
-    {
-      //obj.push(new Glossaire(res[0]));
-      //obj.push(new Glossaire(res[1]));
-      //obj.push(new Glossaire(res[2]));
-      //console.log(res[0].name);
-      //definitions = res;
-      //for (let i=0 ; i < res.length ; i++){
-        //definitions[i] = new Glossaire(res[i].id, res[i].name, res[i].description);
-        //name = res[0].name;
-        //obj.push(res);
-        //console.log(definitions[i]); 
-      //}
-
-      //console.log(definitions);
-    });*/
-    //console.log(obj);
-    //console.log(obj.length);
-    //console.log(obj[0].name);
-    //console.log(name);
-    //console.log(definitions);
-    /*console.log(definitions.forEach(function(el)
-    {
-      console.log(el);
-    }));*/
-
-    /*for (let i = 0 ; i < definitions.length ; i++)
-    {
-      console.log(definitions[i].name);
-    }*/
-
-    //console.log(glossaire.getJSON());
     this.glossaireService = glossaire;
     this.definitions = [];
   }
@@ -90,6 +24,10 @@ export class GlossaireComponent implements OnInit {
     this.glossaireService.getJSON().subscribe(
       (param : Glossaire[]) => {
         this.definitions = param;
+        this.definitions.sort(function(a,b)
+        {
+          return a.name.localeCompare(b.name);
+        });
       }
     );
   }
